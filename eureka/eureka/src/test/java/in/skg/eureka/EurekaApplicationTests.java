4b43d107f16623622205a3ecd60cd6cb4cf749db
shubhamgajare22 <shubhamgajare777@gmail.com>
2024-11-02 23:14:56 +0530

    initial commit food app devleopement branch to main branch git commit -m

---
+++ eureka/eureka/src/test/java/in/skg/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package in.skg.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
